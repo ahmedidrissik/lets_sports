1d2b1f1280ab4e09af82a572d6b7613d736040b2
ahmed idrissi kaitouni <ahmedidrissik@gmail.com>
2022-03-30 12:31:18 +0000

    new game form

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   resources :users, only: [:show, :update]
-  resources :games, only: [:index, :create, :show, :update, :delete]
+  resources :games, only: [:index, :new, :create, :show, :update, :delete]
   resources :bookings, only: [:create, :delete]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
