96aa68cc261064f45d8075702fdeb821cc7ef8a5
ahmed idrissi kaitouni <ahmedidrissik@gmail.com>
2022-04-01 14:48:56 +0000

    games show js and reservation in db thanks zakarya

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -14,3 +14,55 @@ ActiveStorage.start()
 
 import "controllers"
 import "bootstrap"
+
+import Swal from 'sweetalert2';
+document.getElementById('confirmation').addEventListener('click', (event) => {
+  const gameId = document.getElementById('game-data').dataset.gameId;
+  const playerId = document.getElementById('game-data').dataset.playerId;
+  const csrfToken = document.getElementsByName('csrf-token')[0].content;
+  fetch('/bookings', {
+    method: 'POST',
+    headers: {
+      "X-CSRF-Token": csrfToken,
+      'Accept': 'text/plain',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      game_id: gameId,
+      player_id: playerId,
+      status: 'en attente',
+    })
+  }).then((response) => response.text()).then((data) => {
+    document.getElementById("game-details").innerHTML = data;
+    document.getElementById("confirmation").classList.add("d-none")
+    document.getElementById("annulation").classList.remove("d-none")
+    Swal.fire(
+      'Good job!',
+      'You clicked the button!',
+      'success'
+    );
+  })
+})
+
+document.getElementById('annulation').addEventListener('click', (event) => {
+  const bookingId = document.getElementById('game-data').dataset.bookingId;
+  const csrfToken = document.getElementsByName('csrf-token')[0].content;
+  fetch(`/bookings/${bookingId}`, {
+    method: 'DELETE',
+    headers: {
+      "X-CSRF-Token": csrfToken,
+      'Accept': 'text/plain',
+      'Content-Type': 'application/json'
+    },
+
+  }).then((response) => response.text()).then((data) => {
+    document.getElementById("game-details").innerHTML = data;
+    document.getElementById("confirmation").classList.remove("d-none")
+    document.getElementById("annulation").classList.add("d-none")
+    Swal.fire(
+      'Réservation annulée',
+      'à une prochaine fois !',
+      'success'
+    );
+  })
+})
