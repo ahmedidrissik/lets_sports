da4120be58882552269d041245975c3b1057a83a
ahmed idrissi kaitouni <ahmedidrissik@gmail.com>
2022-04-06 17:12:30 +0000

    request

--- db/schema.rb
+++ db/schema.rb
@@ -49,7 +49,7 @@ ActiveRecord::Schema.define(version: 2022_04_05_162034) do
     t.integer "player_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.bigint "event_id", null: false
+    t.bigint "event_id"
     t.index ["event_id"], name: "index_bookings_on_event_id"
     t.index ["game_id"], name: "index_bookings_on_game_id"
   end
@@ -106,7 +106,7 @@ ActiveRecord::Schema.define(version: 2022_04_05_162034) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "name"
-    t.integer "rating"
+    t.integer "rating", default: 0
     t.string "phone_number"
     t.integer "age"
     t.text "bio"
