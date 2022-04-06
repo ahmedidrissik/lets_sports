a6df921be436bae9680d3493bf90ba048675a564
karim chlih <linasanet@gmail.com>
2022-04-06 11:45:02 +0000

    color star

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 2022_04_05_133306) do
+ActiveRecord::Schema.define(version: 2022_04_05_162034) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -107,7 +106,7 @@ ActiveRecord::Schema.define(version: 2022_04_05_133306) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "name"
-    t.integer "rating", default: 0
+    t.integer "rating"
     t.string "phone_number"
     t.integer "age"
     t.text "bio"
